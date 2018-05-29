7b19b40a7fe0d1ef6df1383d28a76d53041f055e
Jose Mena <josemena0114@gmail.com>
2018-05-29 17:59:22 -0400

    hero-title fixed

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,24 +7,28 @@ import '.././styles/normalize.css';
 
 
 const Landing = () => (
-                       <section className="landing">
-                       <h1 className="hero-title">Turn the music up!</h1>
-                       
+            <body className="landing">
                        <section className="hero-content">
-                       <div className="point">
-                       <h2 className="point-title">Choose your music</h2>
-                       <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
-                       </div>
-                       <div className="point">
-                       <h2 className="point-title">Unlimited, streaming, ad-free</h2>
-                       <p className="point-description">No arbitrary limits. No distractions.</p>
-                       </div>
-                       <div className="point">
-                       <h2 className="point-title">Mobile enabled</h2>
-                       <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
-                       </div>
+                         <h1 className="hero-title">Turn the music up!</h1>
                        </section>
+                       <section className="selling-points container">
+                         <div className="point column third">
+                            //<span className="ion-music-note"></span>
+                            <h2 className="point-title">Choose your music</h2>
+                            <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
+                          </div>
+                          <div className="point column third">
+                            //<span className="ion-radio-waves"></span>
+                            <h2 className="point-title">Unlimited, streaming, ad-free</h2>
+                            <p className="point-description">No arbitrary limits. No distractions.</p>
+                          </div>
+                          <div className="point column third">
+                            //<span className="ion-iphone"></span>
+                            <h2 className="point-title">Mobile enabled</h2>
+                            <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
+                          </div>
                        </section>
+             </body>
                        );
 
 export default Landing;
